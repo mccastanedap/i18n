7ebfa5f97879972da3e821ffb179194c26a71bf7
Melissa <mc.castanedap@uniandes.edu.co>
2021-11-29 21:50:05 -0500

    Se hizo el ejercicio

--- src/components/job.js
+++ src/components/job.js
@@ -1,4 +1,6 @@
 import React from "react";
+import { FormattedDate, FormattedMessage } from "react-intl";
+import { FormattedNumber } from "react-intl";
 
 const Job = (props) => {
   return (
@@ -6,9 +8,22 @@ const Job = (props) => {
       <th scope="row">{props.offer.id}</th>
       <td>{props.offer.name}</td>
       <td>{props.offer.company}</td>
-      <td>{props.offer.salary}</td>
+      <td>
+        {props.offer.salary} <FormattedMessage id="Millions" />
+      </td>
       <td>{props.offer.city}</td>
-      <td>{props.offer.date}</td>
+      <td>
+        <FormattedDate
+          value={new Date(props.offer.date)}
+          year="numeric"
+          month="long"
+          day="numeric"
+          weekday="long"
+        />
+      </td>
+      <td>
+        <FormattedNumber value={props.offer.views} />
+      </td>
     </tr>
   );
 };
--- src/index.js
+++ src/index.js
@@ -1,6 +1,20 @@
 import React from "react";
 import ReactDOM from "react-dom";
+import { IntlProvider } from "react-intl";
 
 import JobsList from "./components/jobslist";
+import localeEsMessages from "./locales/es";
+import localeEsMessagesEn from "./locales/en";
 
-ReactDOM.render(<JobsList />, document.getElementById("root"));
+const language = "es";
+console.log(language);
+const messages = {
+  es: localeEsMessages,
+  en: localeEsMessagesEn,
+};
+ReactDOM.render(
+  <IntlProvider locale={language} messages={messages[language]}>
+    <JobsList />
+  </IntlProvider>,
+  document.getElementById("root")
+);
